89b0a42e85eff6e670a2d564ddc0d9c44bd4bb34
vijayalakshmig302 <vijayalakshmig302@gmail.com>
2025-03-29 23:08:53 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,8 +4,10 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, vijayalakshmi\n";
+  cout << "Hello, pavitra\n";
   cout << "Hello, PES University\n";
+  cout << "Hello,PES1UG22CS412\n";
+  
   cout << "I have successfully built and run this  \n";
   return 0;
 }
